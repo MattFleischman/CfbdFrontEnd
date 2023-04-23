a8910b1f2413e2b7e11ae0782f33a6af96a3dd96
mattf <mattfleischman@hotmail.com>
2023-04-23 15:37:06 -0700

    Adding logic to avoid displaying groups if there are no related matchups for a given period

--- src/card_components/ConfGroupings.js
+++ src/card_components/ConfGroupings.js
@@ -3,10 +3,19 @@ import Divider from '@mui/material/Divider';
 import Carousel from 'react-material-ui-carousel'; // see:https://github.com/Learus/react-material-ui-carousel/blob/master/README.md
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
+import {useState, useEffect} from 'react';
 
 
 export default function ConfGroupings(props) {
 
+      const [emptyDetails, setEmptyDetails] = useState(false)
+
+      useEffect( () => {
+      console.log(`${props.groupVal} line details length: ${props.lineDetails.length}`)
+      if (props.lineDetails.length == 0) {
+      setEmptyDetails(true)
+        }
+      }, [props])
 
       console.log("running match Pairing")
       const sliderItems: number = props.lineDetails.length > 4 ? 4 : props.lineDetails.length;
@@ -22,12 +31,11 @@ export default function ConfGroupings(props) {
                       style={{
                         justifyContent: 'center',
                         minHeight: 600,
-                        maxHeight: 700,
-                        marginLeft: 5
+                        maxHeight: 700
                        }}
                         >
                 {props.lineDetails.slice(i, i + sliderItems).map((line) => {
-                  return(<Grid item xs={2.75}>
+                  return(<Grid item xs={3.5}>
                             <TeamComparison lineDetails = {line} />
                         </Grid>);
                 })}
@@ -38,6 +46,7 @@ export default function ConfGroupings(props) {
 
     return(
         <div>
+            {!emptyDetails &&
              <div className="conferenceGroup">
                 <header className="App-header">
                  <Divider variant='middle' orientation="horizontal" flexItem sx={{color: 'grey.200', bgcolor: '#336699'}}>
@@ -45,15 +54,16 @@ export default function ConfGroupings(props) {
                  </Divider>
                  </header>
                         <Carousel
-                        autoPlay
-                        navButtonsAlwaysVisible
-                        interval={8000}
-                        duration={1000}
-                        cycleNavigation={true}
-                        >
-                          {matchups}
+                            autoPlay
+                            navButtonsAlwaysVisible
+                            interval={8000}
+                            duration={1000}
+                            cycleNavigation={true}
+                            >
+                              {matchups}
                         </Carousel>
              </div>
+             }
         </div>
     )
 }
